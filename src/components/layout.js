96f1a6ec91e4f2f8484c1089c8dc4ff2177fe9cb
Isaac Tait <isaac@mountaintopcoding.com>
2021-05-30 19:57:12 -0700

    First Commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,65 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
-const Layout = ({ location, title, children }) => {
+import { GatsbyImage } from "gatsby-plugin-image";
+
+const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
   let header
 
-  if (isRootPath) {
+  const data = useStaticQuery(graphql`{
+  desktopLogo: file(absolutePath: {regex: "/FfT_Logo_Desktop.png/"}) {
+    childImageSharp {
+      gatsbyImageData(
+        width: 500
+        quality: 50
+        placeholder: TRACED_SVG
+        formats: [AUTO, WEBP, AVIF]
+        layout: CONSTRAINED
+      )
+    }
+  }
+  mobileLogo: file(absolutePath: {regex: "/FfT_Logo_Mobile.png/"}) {
+    childImageSharp {
+      gatsbyImageData(
+        width: 250
+        quality: 100
+        placeholder: BLURRED
+        formats: [AUTO, WEBP, AVIF]
+        layout: CONSTRAINED
+      )
+    }
+  }
+}
+`)
+
+const logos = [
+  data.mobileLogo.childImageSharp.gatsbyImageData,
+  {
+    ...data.desktopLogo.childImageSharp.gatsbyImageData,
+    media: `(min-width: 768px)`
+  }
+]
+
+  if (location.pathname === rootPath) {
     header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
+      <div>
+        <GatsbyImage image={logos} alt="Fallfish Tenkara" />
+      </div>
     )
   } else {
     header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
+      <div>
+        <Link to={`/`}>
+          <GatsbyImage image={logos} alt="Fallfish Tenkara" />
+        </Link>
+      </div>
+        
     )
   }
-
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+    <div>
+      <header className="">{header}</header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
