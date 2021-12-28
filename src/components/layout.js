cf6a936b35d0b586cd180691490ea9e9fccf541a
Isaac Tait <isaac@mountaintopcoding.com>
2021-12-28 10:12:57 -0500

    update minimum repo to gatsby 4.4

--- src/components/layout.js
+++ src/components/layout.js
@@ -61,11 +61,14 @@ const logos = withArtDirection(getImage(data.mobileLogo), [
     <div class="flex flex-col">
         <header class="pr-2">{header}</header>  
           <div class="hidden md:block w-2/3 mx-auto">
+          <p><span role="img" aria-label="bipoc pointing finger">&#128070;</span>Improperly displayed logo</p>
       </div>
       
       <hr />
-      <p>How it should look...</p>
+      <p>How the logo should look...<span role="img" aria-label="bipoc pointing finger">&#128071;</span></p>
       <img src={Render} alt="live home screen" />
+      <p>If you open up dev tools and switch from mobile to desktop logo you can see that the mobile logo renders in mobile but when switching back to desktop the desktop logo renders in the same size/dimensions as the mobile logo...</p>
+      <p>Logos can be found at content -> assets</p>
       <div class="bg-gray-200 mb-4 w-full lg:w-2/3 mx-auto overflow-hidden rounded-lg shadow-xl">
         <main>{children}</main>
       </div>
